0b3bf3cdd39db1ec14fc2031d5dd0c8804d019df
Artur Akhmadullin <arturakk2015@gmail.com>
2022-11-25 21:50:09 +0400

    added words & styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+import { words } from "common/words";
 import { Ground } from "components/Ground";
 import { Keyboard } from "components/Keyboard";
 import React, { useState } from "react";
 
 function App() {
   const [word, setWord] = useState("");
-  const [attempt] = useState(1);
+  const [attempt, setAttempt] = useState(1);
+
   const getLetter = (letter: string) => {
     if (word.length < attempt * 5) {
       setWord((prev) => prev + letter);
     }
+  };
+
+  const backSpace = () => {
+    if (word.length > (attempt - 1) * 5) {
+      setWord((prev) => prev.slice(0, prev.length - 1));
+    }
+  };
+
+  const checkWord = () => {
+    const currentWord = word.slice(word.length - 5);
+    if (words.includes(currentWord)) {
+      console.log("НАШЁЛ!");
+    } else {
+      console.log("Подумай ещё");
+    }
+    setAttempt((prev) => prev + 1);
   };
 
   // TODO: Добавить поддержку клавиатуры
@@ -21,9 +39,15 @@ function App() {
 
   return (
     <div className="App">
-      <h3>5БУКВ</h3>
+      <h3>5 Б У К В</h3>
+      <p>Попытка {attempt}</p>
       <Ground word={word} />
-      <Keyboard getLetter={getLetter} />
+      <Keyboard
+        getLetter={getLetter}
+        backSpace={backSpace}
+        checkWord={checkWord}
+        checkIsAvailable={!!word && !(word.length % (5 * attempt))}
+      />
     </div>
   );
 }
--- src/components/Keyboard/index.tsx
+++ src/components/Keyboard/index.tsx
@@ -2,9 +2,17 @@ import "./index.css";
 
 interface IProps {
   getLetter: (letter: string) => void;
+  backSpace: () => void;
+  checkWord: () => void;
+  checkIsAvailable: boolean;
 }
 
-export const Keyboard: React.FC<IProps> = ({ getLetter }) => {
+export const Keyboard: React.FC<IProps> = ({
+  getLetter,
+  backSpace,
+  checkWord,
+  checkIsAvailable,
+}) => {
   const upperRow = ["й", "ц", "у", "к", "е", "н", "г", "ш", "щ", "з", "х", "ъ"];
   const middleRow = ["ф", "ы", "в", "а", "п", "р", "о", "л", "д", "ж", "э"];
   const lowerRow = ["я", "ч", "с", "м", "и", "т", "ь", "б", "ю"];
@@ -26,13 +34,25 @@ export const Keyboard: React.FC<IProps> = ({ getLetter }) => {
         ))}
       </div>
       <div className="keyboard__row">
-        <div className=" key key__gray keyboard__buttons">✓</div>
+        <div
+          className={`key key__gray keyboard__buttons ${
+            checkIsAvailable ? "key__yellow" : ""
+          }`}
+          onClick={() => checkIsAvailable && checkWord()}
+        >
+          ✓
+        </div>
         {lowerRow.map((letter) => (
           <div className="key" key={letter} onClick={() => getLetter(letter)}>
             {letter}
           </div>
         ))}
-        <div className=" key key__gray keyboard__buttons">✗</div>
+        <div
+          className="key key__gray keyboard__buttons"
+          onClick={() => backSpace()}
+        >
+          ✗
+        </div>
       </div>
     </div>
   );
